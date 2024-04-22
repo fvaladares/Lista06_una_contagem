1d89af057cc7154f978b4d7bea3fb6b0feb919ef
Fabricio Valadares <fabricio.valadares@gmail.com>
2024-04-22 20:47:24 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+
+ int x, y, z, k, l;
+        char op;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Informe a operaçao desjada: ");
+        x = s.nextInt();
+        op = s.next().charAt(0);
+        y = s.nextInt();
+        System.out.println();
+        System.out.println(x + op + y);
+
+        switch (op) {
+            case '+':
+                System.out.println("Adiçao");
+                break;
+            case '-':
+                System.out.println("Subtraçao");
+                break;
+            default:
+                System.out.println("NAO SEI...");
+                break;
+        }
+ */
